335e5e51da1229d80f788558b65f43b9a21f1d0e
Benjamin Picquet <benjamin.picquet@hotmail.com>
2020-11-11 15:24:45 +0100

    ajout design de la page login et register

--- vues/login.php
+++ vues/login.php
@@ -1,11 +1,24 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion et inscription</title>
-</head>
-<body>
-    <p>dsfrgdrttt</p>
-</body>
-</html>
+<section class="login-page">
+    <div class="login">
+        <h2>Connexion</h2>
+
+        <form action="">
+            <input type="text" name="username" placeholder="Pseudo" required="" autofocus="" />
+            <input type="password" name="password" placeholder="Password" required="" autofocus="" />
+            <button class="all-button button-login" type="submit">Se connecter</button>
+        </form>
+    </div>
+
+    <img src="./style/img/logo-mahora.png" alt="">
+
+    <div class="register">
+        <h2>Inscription</h2>
+
+        <form action="">
+            <input type="text" name="username" placeholder="Pseudo" required="" autofocus="" />
+            <input type="password" name="password" placeholder="Password" required="" autofocus="" />
+            <button class="all-button button-login" type="submit">Se connecter</button>
+        </form>
+    </div>
+</section>
+    
