4502451b25176d46e81e4c7eaf9d4ed9ad4d3a5d
Benjamin Picquet <benjamin.picquet@hotmail.com>
2020-11-20 12:03:00 +0100

    add form accueil

--- vues/accueil.php
+++ vues/accueil.php
@@ -3,17 +3,33 @@ if(!isset($_SESSION["id"])) {
         // On n est pas connecté, il faut retourner à la page de login
         header("Location:index.php?action=login");
 }
-
-
 ?>
 <section class="accueil-post">
 <?php
     $mypostsql = "SELECT * FROM ecrit INNER JOIN user ON ecrit.idAuteur = user.id  ORDER BY dateEcrit DESC";
-
     $query = $pdo->prepare($mypostsql);
-
     $query->execute([]);
+    ?>
 
+    <div class="add-post div-post">
+        <div>
+            <?php
+            if($line = $query->fetch()) {
+                ?>
+                <img src="<?= $line["avatar"]?>" alt="">
+                <h3><?= $line["login"]?></h3>
+                <?php
+            }
+            ?>
+        </div>
+        <form action="index.php?action=add-post.php" method="post">
+            <input type="text" name="titre" placeholder="Titre" required="" autofocus="" />
+            <input type="text" name="contenu" placeholder="Votre publication" required="" autofocus="" />
+            <button class="all-button button-login" name="formsendpublication" type="submit">Publier</button>
+        </form>
+    </div>
+
+    <?php
     while($line = $query->fetch()) {
         ?>
         <div class="div-post">
