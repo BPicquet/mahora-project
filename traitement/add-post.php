54557db0b7edd0300c6b8ffaf9df92ca4f2cbf7f
Duval Julien <julienduval17@gmail.com>
2020-11-20 12:19:15 +0100

    add-post

--- traitement/add-post.php
+++ traitement/add-post.php
@@ -1,3 +1,21 @@
 <?php
+    $titre = $_POST['titre'];
+    $contenu = $_POST['contenu'];
+    $id = $_GET["id"];
 
+    if(!empty($titre) && !empty($contenu)){
+        $query = $pdo->prepare('INSERT INTO ecrit(titre, contenu, idAuteur, idAmi) VALUES(:titre, :contenu, :idAuteur, :idAmi)');
+        $query->execute(array(
+            'titre' => $titre,
+            'contenu' => $contenu,
+            'idAuteur' => $id,
+            'idAmi' => $id));
+        echo "Votre compte à été créé";
+        header('Location: index.php?action=accueil');
+        exit();   
+        
+    } 
+    else{
+        echo "Veuillez renseigner les champs";
+    }
 ?>
