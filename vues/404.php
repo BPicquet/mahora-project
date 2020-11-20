ae21e8b0df883acc2ae2cec51608e45fcca33dce
Benjamin Picquet <benjamin.picquet@hotmail.com>
2020-11-20 11:12:56 +0100

    add

--- vues/404.php
+++ vues/404.php
@@ -3,7 +3,7 @@
         <h2>Error 404</h2>
         <div>
             <h3>Page not found</h3>
-            <button class="all-button">Home</button>
+            <a href="index.php?action=accueil&id=<?php echo $_SESSION['id'] ?>"><button class="all-button">Home</button></a>
         </div>
     </div>
 </section>
